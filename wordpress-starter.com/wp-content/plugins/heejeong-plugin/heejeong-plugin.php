907159cb6426baec95954a3be67f012de9267664
Heejeong Cho <w0424547@nscc.ca>
2020-04-05 12:34:03 -0300

    Create a custom plugin using scaffolding with wp-cli

---
+++ wordpress-starter.com/wp-content/plugins/heejeong-plugin/heejeong-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Heejeong Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Heejeong Cho
+ * Text Domain:     heejeong-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Heejeong_Plugin
+ */
+
+// Your code starts here.
