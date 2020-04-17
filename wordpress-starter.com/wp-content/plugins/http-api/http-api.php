c5b4f02d2ee144a7789b6e92388c2bc9b240cb47
Heejeong Cho <w0424547@nscc.ca>
2020-04-17 04:24:41 -0300

    Create Http API plugin

---
+++ wordpress-starter.com/wp-content/plugins/http-api/http-api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Http Api
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     http-api
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Http_Api
+ */
+
+// Your code starts here.
