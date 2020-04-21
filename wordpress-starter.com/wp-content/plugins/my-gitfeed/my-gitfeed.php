22dece726920d914b1776cdb02435f6b97c7307d
Heejeong Cho <w0424547@nscc.ca>
2020-04-21 15:52:34 -0300

    Create GIthub Feed Plugin

---
+++ wordpress-starter.com/wp-content/plugins/my-gitfeed/my-gitfeed.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     My Github Feed
+ * Description:     WEBD3027 Project 2: Plugin Development
+ * Author:          Heejeong Cho
+ * Author URI:      heejeong.com
+ * Text Domain:     my-gitfeed
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         My_Gitfeed
+ */
+
